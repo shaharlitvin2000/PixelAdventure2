0b128d4d8ec4638dbbed7c82225c4085b596f176
shaharlitvin2000 <shahar070510@gmail.com>
2026-02-16 20:51:51 +0200

    clean project

---
+++ Assets/PlayerMovment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
