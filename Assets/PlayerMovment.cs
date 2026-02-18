f1cfcaf00b93a19b5e2a1f858d1ae6f1d3c0b804
shaharlitvin2000 <shahar070510@gmail.com>
2026-02-18 15:33:21 +0200

    Added player Animations

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -1,25 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    private void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
         rb.velocity = moveInput * moveSpeed;
     }
 
-    private void OnMove(InputValue value)
+    public void Move(InputAction.CallbackContext context)
     {
-        moveInput = value.Get<Vector2>();
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
